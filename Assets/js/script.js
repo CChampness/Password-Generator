763c1c38d598c1242d6b267cde2b817eee66cfba
CChampness <87551272+CChampness@users.noreply.github.com>
2021-09-12 21:14:40 -0400

    Revised algorithm

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,51 +13,93 @@ function getPWLength() {
 }
 
 function getTypeSelections() {
-  return window.prompt("Enter ULNS");
-}
-
-function getRandom(typeSel) {
-  var types = "";
-
-  if (typeSel == 'u') {
-    types = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if (typeSel == 'l') {
-    types = "abcdefghijklmnopqrstuvwxyz";
-  } else if (typeSel == 'n') {
-    types = "0123456789";
-  } else {
-    types = "!#$%^&*()?{}[]";
+  // return window.prompt("Enter ULNS");
+  /* Could use confirms for all 4 types */
+  var typeList = "";
+  if (confirm("Upper case?")) {
+    typeList += "u";
   }
 
-   var ranChar = types[Math.floor(Math.random() * types.length)];
+  if (confirm("Lower case?")) {
+    typeList += "l";
+  }
+
+  if (confirm("Special characters?")) {
+    typeList += "s";
+  }
+
+  if (confirm("Numbers?")) {
+    typeList += "n";
+  }
+  
+  return typeList;
+}
+
+function getRandom(charList) {
+   var ranChar = charList[Math.floor(Math.random() * charList.length)];
    return ranChar;
 }
 
-function populatePassword(pwLen, typeSel) {
-  var pwBuf;
-  var typePos = 0;
+function populatePassword(pwLen, charList) {
+ 
+  // var typePos = 0;
   var newChar;
   var pw = "";
 
   for (i = 0; i < pwLen; i++) {
-      if (typePos >= typeSel.length) {
-      typePos = 0;
+    newChar = getRandom(charList);
+    pw = pw + newChar;
+      // if (typePos >= typeSel.length) {
+      // typePos = 0;
     }
 
-    newChar = getRandom(typeSel[typePos]);
-    pw = pw + newChar;
-    typePos++;
-  }
+    // newChar = getRandom(typeSel[typePos]);
+    // pw = pw + newChar;
+    // typePos++;
+  // }
   return pw;
 }
+
+function buildCharList(typeSel) {
+  var charList = "";
+
+  for (var i = 0; i < typeSel.length; i++) {
+    var type = typeSel[i];
+
+    if (type == 'u') {
+      charList += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } 
+
+    if (type == 'l') {
+      charList += "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    if (type == 'n') {
+      charList += "0123456789";
+    }
+
+    if (type == 's') {
+      charList += "!@#$%^&*(){}|[]\/?<>";
+    }
+  }
+  return charList;
+}
+
+// build a temporary string that has the combined types,
+// then select randomly from the temp string.
 
 // Create a pseudo-random password with user's input
 function generatePassword() {
   var pwLength = getPWLength();
   var typeSelections = getTypeSelections();
-  var pw = populatePassword(pwLength, typeSelections);
+  console.log(typeSelections);
+  var charList = buildCharList(typeSelections);
+  console.log(charList);
+  var pw = populatePassword(pwLength, charList);
   return pw;
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -69,6 +111,9 @@ function writePassword() {
     generateBtn.removeEventListener("click", writePassword);
 }
 
+/* <button aria-hidden="false" type="button" class="copy-icon">
+  <span class="visually-hidden">Copy to Clipboard</span>
+</button> */
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
